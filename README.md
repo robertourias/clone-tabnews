4f3e1a5d5ec1e35509a90df2ab800b08fbe86deb
Roberto Miyazaki Urias Nicoletti <roberto.urias@gmail.com>
2024-11-27 12:38:36 +0000

    Configurar o Prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto Tab news do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Roberto Nicoletti</h1>
+  return <h1>Roberto Nicoletti</h1>;
 }
 
 export default Home;
