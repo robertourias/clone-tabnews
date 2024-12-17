401656a83076950b0fe400067c27a0f50647c283
Roberto Nicoletti <roberto.urias@gmail.com>
2024-12-17 00:03:16 -0300

    criando primeiro teste

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,14 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+test("soma de 2 + 2 dever ser igual a 4", () => {
+  const calculadora = require("../models/calculadora");
+  expect(calculadora.somar(2, 2)).toBe(4);
+});
+
+test("soma de 120 + 30 dever ser igual a 150", () => {
+  const calculadora = require("../models/calculadora");
+  expect(calculadora.somar(120, 30)).toBe(150);
+});
+
+test("soma de banana + 5 dever ser igual a Erro", () => {
+  const calculadora = require("../models/calculadora");
+  expect(calculadora.somar("banana", 5)).toBe("Erro");
 });
